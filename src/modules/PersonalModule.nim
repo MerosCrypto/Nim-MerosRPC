9c52db1181462c459fcb73f75bbcdadfd37b27f6
Luke Parker <lukep@embercrypto.io>
2019-01-10 20:38:15 -0600

    Add personal.data.

--- src/modules/PersonalModule.nim
+++ src/modules/PersonalModule.nim
@@ -4,17 +4,11 @@ import ../EmberRPCObj
 #Async standard lib.
 import asyncdispatch
 
+#String utils standard lib.
+import strutils
+
 #JSON standard lib.
 import JSON
-
-#Get the Wallet.
-proc getWallet*(personal: PersonalModule): Future[JSONNode] {.async.} =
-    #Call getWallet and store the result.
-    result = await personal.parent.call("personal", "getWallet")
-
-    #If there was an error, raise it.
-    if result.hasKey("error"):
-        raise newException(EmberError, result["error"].getStr())
 
 #Set the seed.
 proc setSeed*(personal: PersonalModule, seed: string) {.async.} =
@@ -26,6 +20,15 @@ proc setSeed*(personal: PersonalModule, seed: string) {.async.} =
     #If there was an error, raise it.
     if res.hasKey("error"):
         raise newException(EmberError, res["error"].getStr())
+
+#Get the Wallet.
+proc getWallet*(personal: PersonalModule): Future[JSONNode] {.async.} =
+    #Call getWallet and store the result.
+    result = await personal.parent.call("personal", "getWallet")
+
+    #If there was an error, raise it.
+    if result.hasKey("error"):
+        raise newException(EmberError, result["error"].getStr())
 
 #Create a Send.
 proc send*(
@@ -49,7 +52,7 @@ proc send*(
     result = res["hash"].getStr()
 
 #Create a Receive.
-proc send*(
+proc receive*(
     personal: PersonalModule,
     inputAddress: string,
     inputNonce: string,
@@ -59,6 +62,25 @@ proc send*(
     var res: JSONNode = await personal.parent.call("personal", "receive", %* [
         inputAddress,
         inputNonce,
+        nonce
+    ])
+
+    #If there was an error, raise it.
+    if res.hasKey("error"):
+        raise newException(EmberError, res["error"].getStr())
+
+    #Else, return the hash.
+    result = res["hash"].getStr()
+
+#Create a Data.
+proc data*(
+    personal: PersonalModule,
+    data: string,
+    nonce: string
+): Future[string] {.async.} =
+    #Call data.
+    var res: JSONNode = await personal.parent.call("personal", "data", %* [
+        data.toHex(),
         nonce
     ])
 
