a8d1a44f3665a812290705e699465e9c929cef79
Luke Parker <lukep@embercrypto.io>
2019-01-12 14:00:44 -0600

    Add `getUnarchivedVerifications`.

--- MerosRPC/modules/VerificationsModule.nim
+++ MerosRPC/modules/VerificationsModule.nim
@@ -9,3 +9,12 @@ import strutils
 
 #JSON standard lib.
 import JSON
+
+#Get unarchived verifications.
+proc getUnarchivedVerifications*(verifs: VerificationsModule): Future[JSONNode] {.async.} =
+    #Call getUnarchivedVerifications and store it in the result.
+    result = await verifs.parent.call("verifs", "getUnarchivedVerifications")
+
+    #If there was an error, raise it.
+    if result.hasKey("error"):
+        raise newException(MerosError, result["error"].getStr())
