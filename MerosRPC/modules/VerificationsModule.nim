af54f712ea00a4d163b864a424736a04c82ea8b7
Luke Parker <lukep@embercrypto.io>
2019-01-16 10:39:10 -0600

    Add support for getVerification.

--- MerosRPC/modules/VerificationsModule.nim
+++ MerosRPC/modules/VerificationsModule.nim
@@ -10,6 +10,23 @@ import strutils
 #JSON standard lib.
 import JSON
 
+#Get a verification.
+proc getVerification*(
+    verifs: VerificationsModule,
+    key: string,
+    nonce: uint
+): Future[JSONNode] {.async.} =
+    #Call getVerification and store it in the result.
+    result = await verifs.parent.call("verifications", "getVerification", %* [
+        key,
+        int(nonce)
+    ])
+
+    #If there was an error, raise it.
+    if result.kind == JObject:
+        if result.hasKey("error"):
+            raise newException(MerosError, result["error"].getStr())
+
 #Get unarchived verifications.
 proc getUnarchivedVerifications*(verifs: VerificationsModule): Future[JSONNode] {.async.} =
     #Call getUnarchivedVerifications and store it in the result.
