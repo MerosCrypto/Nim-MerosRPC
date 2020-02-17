d6fae739cdc05503d3b76f3904dd5045ba8dd565
Luke Parker <lukeparker5132@gmail.com>
2020-02-17 01:05:13 -0600

    Redo the Meros RPC library.
    This is a drop-in replacement built around auto-generating every route 
    via a macro. It's meant to completely cleanup the existing library.

--- MerosRPC.nim
+++ MerosRPC.nim
@@ -1,18 +1,260 @@
-#RPC object.
-import MerosRPC/MerosRPCObj
-export MerosRPCObj
+#Async standard lib.
+import asyncdispatch
 
-#Modules.
-import MerosRPC/modules/SystemModule
-import MerosRPC/modules/MeritModule
-import MerosRPC/modules/ConsensusModule
-import MerosRPC/modules/TransactionsModule
-import MerosRPC/modules/PersonalModule
-import MerosRPC/modules/NetworkModule
+#Networking standard lib.
+import asyncnet
 
-export SystemModule
-export MeritModule
-export ConsensusModule
-export TransactionsModule
-export PersonalModule
-export NetworkModule
+#String utils standard lib.
+import strutils
+
+#JSON standard lib.
+import json
+
+#Macros standard lib.
+import macros
+
+type
+    #Exception used for when the Meros RPC returns an error.
+    MerosError* = object of Exception
+        code: int
+
+    #Module objects.
+    Module* = ref object of RootObj
+        parent*: MerosRPC
+
+    SystemModule*       = ref object of Module
+    MeritModule*        = ref object of Module
+    ConsensusModule*    = ref object of Module
+    TransactionsModule* = ref object of Module
+    NetworkModule*      = ref object of Module
+    PersonalModule*     = ref object of Module
+
+    #RPC object.
+    MerosRPC* = ref object
+        #IP/Port.
+        ip*:   string
+        port*: int
+
+        #Socket.
+        socket*: AsyncSocket
+
+        #Modules.
+        system*:       SystemModule
+        merit*:        MeritModule
+        consensus*:    ConsensusModule
+        transactions*: TransactionsModule
+        network*:      NetworkModule
+        personal*:     PersonalModule
+
+#Constructors.
+func newMerosError(
+    code: int,
+    msg: string
+): ref MerosError =
+    result = newException(MerosError, msg)
+    result.code = code
+
+proc newMerosRPC*(
+    ip: string = "127.0.0.1",
+    port: int = 5133
+): Future[MerosRPC] {.async.} =
+    #Set the IP/Port and create a Socket.
+    result = MerosRPC(
+        ip: ip,
+        port: port,
+        socket: newAsyncSocket()
+    )
+
+    #Set up the modules.
+    result.system = SystemModule(
+        parent: result
+    )
+    result.merit = MeritModule(
+        parent: result
+    )
+    result.consensus = ConsensusModule(
+        parent: result
+    )
+    result.transactions = TransactionsModule(
+        parent: result
+    )
+    result.network = NetworkModule(
+        parent: result
+    )
+    result.personal = PersonalModule(
+        parent: result
+    )
+
+    #Connect.
+    await result.socket.connect(ip, Port(port))
+
+#Call.
+proc call*(
+    rpc: MerosRPC,
+    module: string,
+    methodName: string,
+    args: JSONNode = %* []
+): Future[JSONNode] {.async.} =
+    #Send the data.
+    await rpc.socket.send($ %* {
+        "jsonrpc": "2.0",
+        "id": 0,
+        "method": module & "_" & methodName,
+        "params": args
+    })
+
+    #Get the response back.
+    var
+        #Response.
+        res: string
+        #Counter used to track if the response is complete.
+        counter: int = 0
+    while true:
+        res &= await rpc.socket.recv(1)
+        if res[^1] == res[0]:
+            inc(counter)
+        elif (res[^1] == ']') and (res[0] == '['):
+            dec(counter)
+        elif (res[^1] == '}') and (res[0] == '{'):
+            dec(counter)
+        if counter == 0:
+            break
+
+    #Return the response.
+    result = parseJSON(res)
+
+    #If it has an error, raise and return it.
+    if result.hasKey("error"):
+        raise newMerosError(result["error"]["code"].getInt(), result["error"]["msg"].getStr())
+
+#Route macro which automatically expands every below definition to a proper function.
+macro route(
+    moduleType: typedesc,
+    moduleStr: string,
+    functions: untyped
+): untyped =
+    result = newStmtList()
+
+    var f: int = 0
+    while f < functions.len:
+        var
+            function: NimNode = functions[f]
+            params: seq[NimNode] = @[
+                newNimNode(nnkBracketExpr).add(ident("Future"), function[^1][0]),
+                newIdentDefs(ident("module"), ident(moduleType.strVal))
+            ]
+            args: NimNode = newNimNode(nnkBracket)
+            body: NimNode = newStmtList()
+
+        for p in 1 ..< function.len - 1:
+            if function[p].kind == nnkPrefix:
+                function[p] = function[p][1]
+                functions.add(functions[f])
+                break
+            elif function[p].kind == nnkIdent:
+                params.add(
+                    newIdentDefs(ident("arg" & $(p - 1)), function[p])
+                )
+                args.add(ident("arg" & $(p - 1)))
+
+                if function[p].strVal == "string":
+                    args[^1] = newCall(ident("toHex"), args[^1])
+            else:
+                doAssert(false, "Unknown route definition.")
+
+        body.add(
+            newNimNode(nnkReturnStmt).add(
+                newNimNode(nnkBracketExpr).add(
+                    newNimNode(nnkCommand).add(
+                        ident("await"),
+                        newCall(
+                            ident("call"),
+                            newDotExpr(ident("module"), ident("parent")),
+                            newStrLitNode(moduleStr.strVal),
+                            newStrLitNode(function[0].strVal),
+                            prefix(args, "%*")
+                        )
+                    ),
+                    newStrLitNode("result")
+                )
+            )
+        )
+
+        case function[^1][0].strVal:
+            of "void":
+                body.del(body.len - 1)
+
+            of "int":
+                body[^1][0] = newCall(ident("getInt"), body[^1][0])
+
+            of "string":
+                body[^1][0] = newCall(
+                    ident("parseHexStr"),
+                    newCall(ident("getStr"), body[^1][0])
+                )
+
+            of "JSONNode":
+                discard
+
+            else:
+                doAssert(false, "Unknown route result type.")
+
+        result.add(newProc(
+            postfix(function[0], "*"),
+            params,
+            body,
+            nnkProcDef,
+            newNimNode(nnkPragma).add(ident("async"))
+        ))
+        inc(f)
+
+#System module.
+route SystemModule, "system":
+    quit(): void
+
+#Merit module.
+route MeritModule, "merit":
+    getHeight(): int
+    getDifficulty(): string
+    getBlock(string): JSONNode
+
+    getTotalMerit(): int
+    getUnlockedMerit(): int
+    getMerit(int): JSONNode
+
+    getBlockTemplate(string): JSONNode
+    publishBlock(int, string): void
+
+#Consensus module.
+route ConsensusModule, "consensus":
+    getSendDifficulty(): string
+    getDataDifficulty(): string
+
+    getStatus(string): JSONNode
+
+#Transactions module.
+route TransactionsModule, "transactions":
+    getTransaction(string): JSONNode
+
+#Network module.
+route NetworkModule, "network":
+    connect(string, ?int): void
+    getPeers(): JSONNode
+
+#Personal module.
+route PersonalModule, "personal":
+    getMiner(): string
+
+    setMnemonic(?string, ?string): void
+    getMnemonic(): string
+
+    send(string, string): string
+    data(string): string
+
+    toAddress(string): string
+
+#Disconnect.
+proc disconnect*(
+    rpc: MerosRPC
+) =
+    rpc.socket.close()
--- MerosRPC.nimble
+++ MerosRPC.nimble
@@ -1,6 +1,6 @@
-version     = "1.4.1"
+version     = "2.0.0"
 author      = "Luke Parker"
 description = "A Nim Library for connecting to the Meros RPC."
 license     = "MIT"
 
-requires "nim >= 0.20.2"
+requires "nim >= 1.0.6"
